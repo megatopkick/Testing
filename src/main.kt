78e5db9621aa0d2d65e6892384b79727269d9a1f
John <43836753+megatopkick@users.noreply.github.com>
2018-10-06 08:53:05 -0600

    Followed Tutorial
    Followed youtube tutorial for reacquaint myself with some Kotlin Syntax.

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,82 @@
+import javax.xml.stream.events.Characters
+
+/** Tutorial by derekbanas on 05/21/17....
+ *  Followed by Megatopkick on 10/06/2018
+ */
+
 
     fun main(args: Array<String>) {
+    /**
     println("Hello World !")
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+
+    println("Biggest Int : " + bigInt)
+    println("Smallest Int : $smallInt")
+
+    var dblNum1: Double =1.1111111111111111
+    var dblNum2: Double =1.1111111111111111
+
+    println("Sum : " + (dblNum1 + dblNum2))
+
+    val myName = "Megatopkick"
+
+    val longStr = """This is a very....
+Long String!!!!"""
+
+
+    var fName= "Mega"
+    var lName: String = "Smith"
+
+    fName = "John"
+
+    var fullName = fName + " " + lName
+
+    println("Name : $fullName")
+
+    println("1 + 2 = ${1+2}")
+
+    println("String Length : ${longStr.length}")
+
+    var str1 = "A random string"
+    var str2 = "a random string"
+
+    println("Strings Equal : ${str1.equals(str2)}")
+    println("Compare A to B : ${'A'.compareTo('B')}")
+
+    println("2nd Index : ${str1[2]}")
+
+    println("Index 2-7 : ${str1.subSequence(2,8)}")
+
+    println("Contains 'random' : ${str1.contains("random")}")
+
+
+
+    var myArray = arrayOf(1, 1.23, "Smith")
+
+    println(myArray[2])
+
+    myArray[1] = 3.14
+
+    println("Array Length : ${myArray.size}")
+
+    println("Smith in Array : ${myArray.contains("Smith")}")
+
+    var partArray = myArray.copyOfRange(0,1)
+
+    println("Smith Index : ${myArray.indexOf("Smith")}")
+
+    var sqArray = Array(5, {x -> x * x})
+
+    println(sqArray[2])
+
+    var arr2: Array<Int> = arrayOf(1,2,3)
+*/
+
+
+
+
+
+
+
 }
